7ed3b5fbe2c506986edbf655955926f17b934ced
yuan tan <dyxtan@gmail.com>
2021-12-22 01:45:30 +1300

    add MatchRound to types

--- client/types.ts
+++ client/types.ts
@@ -2,7 +2,12 @@ interface Match {
   isTop: boolean,
   id1: number,
   id2: number | null,
-  winner: number | null;
+  winner: number
 };
 
-export { Match }
+interface MatchRound {
+  id: number,
+  matches: Match[];
+}
+
+export { Match, MatchRound }
