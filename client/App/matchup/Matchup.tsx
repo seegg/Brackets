64edd90323b1edb547466e22f1937e29deec2535
yuan tan <dyxtan@gmail.com>
2021-12-22 19:04:56 +1300

    set top and bottom gap to be within mathcup arrow

--- client/App/matchup/Matchup.tsx
+++ client/App/matchup/Matchup.tsx
@@ -9,24 +9,39 @@ interface Props extends Match {
 };
 
 const Matchup = ({ id1, id2, winner, isTop, topGap, bottomGap }: Props): JSX.Element => {
-  console.log(topGap);
   return (
     <>
       <div className='matchup'>
         <div>
-          {topGap && <div className='matchup-gap'></div>}
-          <Cell id={id1} isWinner={id1 === winner} />
-          <div className='matchup-gap'></div>
-          <Cell id={id2} isWinner={id2 === winner} />
-          {bottomGap && <div className='matchup-gap'></div>}
-        </div>
-        <div className='arrow-container'>
-          <div className='cell-arrow'>
-            <div className='arrow arrow-top'></div>
-            <div className='arrow arrow-bottom'></div>
+          <div>
+            {(isTop && topGap) && <div className='filler-gap top-gap'></div>}
+          </div>
+          <div className='matchup'>
+            <div >
+              {(!isTop && topGap) && <div className='filler-gap top-gap'></div>}
+              <div className='matchup'>
+                <div>
+                  <Cell id={id1} isWinner={id1 === winner} />
+                  <div className='matchup-gap'></div>
+                  <Cell id={id2} isWinner={id2 === winner} />
+                </div>
+                <div className='arrow-container'>
+                  <div className='cell-arrow'>
+                    <div className='arrow arrow-top'></div>
+                    <div className='arrow arrow-bottom'></div>
+                  </div>
+                </div>
+              </div>
+              {(isTop && bottomGap) && <div className='filler-gap bottom-gap'></div>}
+            </div>
+            {(topGap || bottomGap) && <div className={`matchup-arrow matchup-${isTop ? 'top' : 'bottom'}`}></div>}
+          </div>
+          <div>
+            {(!isTop && bottomGap) && <div className='filler-gap bottom-gap'></div>}
           </div>
         </div>
-        {(topGap || bottomGap) && <div className={`matchup-arrow matchup-${isTop ? 'top' : 'bottom'}`}></div>}
+
+
       </div>
     </>
   );
